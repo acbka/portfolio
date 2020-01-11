0e92fe4105abbf1f15f5930ed3c7888e3afa10b1
acbka <vladochka.vb@gmail.com>
2020-01-11 14:54:12 +1300

    top menu active

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import"./style.scss";
 import 'roboto-fontface';
+
+
+
+function burgerMenu(){
+   document.querySelector(".burger").addEventListener("click", function(){
+      document.querySelector(".top-menu").classList.toggle("show");
+   })
+}
+
+burgerMenu()
