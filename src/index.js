d7f0bfec5f4168dbe8749293bb2d0f01abcd6386
acbka <vladochka.vb@gmail.com>
2020-01-15 21:19:55 +1300

    bicycle

--- src/index.js
+++ src/index.js
@@ -102,32 +102,34 @@ function scrollPage(){
 
 // go to top
 
-let scrollButton = document.querySelector(".scroll_top");
-
 function goToTop() {
+   let scrollButton = document.querySelector(".scroll_top");
+   
    window.addEventListener("scroll", trackScroll);
    scrollButton.addEventListener("click", goUp);
-}
 
-function trackScroll() {
-   let scrolled = window.scrollY + 200;
-   let y = document.documentElement.clientHeight;
-
-   if (scrolled > y) {
-      scrollButton.classList.add("show");
-   } else {
-      scrollButton.classList.remove("show");
+   function trackScroll() {
+      let scrolled = window.scrollY + 200;
+      let y = document.documentElement.clientHeight;
+   
+      if (scrolled > y) {
+         scrollButton.classList.add("show");
+      } else {
+         scrollButton.classList.remove("show");
+      }
+   }
+   
+   function goUp() {
+      if (window.pageYOffset > 0) {
+         window.scrollBy(0, -80);
+         setTimeout(goUp, 0);
+      };
+      document.querySelector(".active").classList.remove("active");
+      document.querySelector('a[href="#home"]').parentNode.classList.add("active");
    }
 }
 
-function goUp() {
-   if (window.pageYOffset > 0) {
-      window.scrollBy(0, -80);
-      setTimeout(goUp, 0);
-   };
-   document.querySelector(".active").classList.remove("active");
-   document.querySelector('a[href="#home"]').parentNode.classList.add("active");
-}
+
 
 // portfolio
 
